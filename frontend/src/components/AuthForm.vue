d8e98bb5a0084a27e1a9cc6a4cd0f1e81b27ced8
dhelfy <dhcode4business@gmail.com>
2026-04-07 16:25:01 +0500

    feat: implemented auth page

---
+++ frontend/src/components/AuthForm.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Button, FloatLabel, InputText, Panel } from 'primevue';
+import { ref } from 'vue';
+
+const apiKey = ref('')
+
+const onLogin = async () => {
+  console.log('Login...')
+}
+</script>
+
+<template>
+  <Panel>
+    <template #header>
+      <h1>Вход</h1>
+    </template>
+
+    <form class="authPanelContent" @submit.prevent="onLogin">
+      <FloatLabel variant="on">
+        <InputText id="on_label" v-model="apiKey" type="text" />
+        <label for="on_label">API Key</label>
+      </FloatLabel>
+      <Button label="Войти" @click="onLogin" />
+    </form>
+  </Panel>
+</template>
+
+<style scoped>
+.p-panel {
+  width: fit-content;
+  height: fit-content;
+}
+
+.authPanelContent {
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  gap: 16px;
+}
+</style>
---
+++ frontend/src/pages/auth.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AuthForm from '@/components/AuthForm.vue';
+
+</script>
+
+<template>
+  <div class="authPage">
+    <AuthForm />
+  </div>
+</template>
+
+<style scoped>
+.authPage {
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+</style>
