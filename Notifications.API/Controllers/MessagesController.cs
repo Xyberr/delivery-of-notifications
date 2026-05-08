5137efee1b90a2097d3ec9f71f52e30e2766afd7
Xyberr <makszacev238@gmail.com>
2026-05-08 13:29:27 +0500

    refactor: праки по ревью - поправил форматирование у GetList

--- Notifications.API/Controllers/MessagesController.cs
+++ Notifications.API/Controllers/MessagesController.cs
@@ -37,7 +37,8 @@ public class MessagesController(IMessageService service) : ControllerBase
     }
 
     [HttpGet]
-    [ProducesResponseType(typeof(ICollection<MessageResponse>), StatusCodes.Status200OK)]    public async Task<IActionResult> GetList(CancellationToken cancellationToken)
+    [ProducesResponseType(typeof(ICollection<MessageResponse>), StatusCodes.Status200OK)]    
+    public async Task<IActionResult> GetList(CancellationToken cancellationToken)
     {
         var result = await service.GetListAsync(cancellationToken);
 
